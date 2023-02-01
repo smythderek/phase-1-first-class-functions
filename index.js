dd951a4c3b2bd9c0cb616d032a97d173dace089e
Derek Smyth <smyth.derek@gmail.com>
2023-02-01 13:31:41 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callBackFunction) {
+    callBackFunction();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    };
+}
